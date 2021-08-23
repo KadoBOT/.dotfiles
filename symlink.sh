f4e71614b71403f515af5a37a1d79d4c07f96e67
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2021-08-23 08:37:52 +0200

    fix symlink folders

--- symlink.sh
+++ symlink.sh
@@ -5,8 +5,8 @@ ln -sfn ${PWD}/.spacemacs ~/.spacemacs
 ln -sfn ${PWD}/.tmux.conf ~/.tmux.conf
 ln -sfn ${PWD}/.zshrc ~/.zshrc
 ln -sfn ${PWD}/nvim/init.vim ~/.config/nvim/init.nvim
-ln -sfn ${PWD}/nvim/lua ~/.config/nvim/lua
-ln -sfn ${PWD}/nvim/UltiSnips ~/.config/nvim/UltiSnips
+ln -sfn ${PWD}/nvim/lua/kadobot/* ~/.config/nvim/lua/kadobot
+ln -sfn ${PWD}/nvim/UltiSnips/* ~/.config/nvim/UltiSnips
 ln -sfn ${PWD}/.i3/config ~/.i3/config
 ln -sfn ${PWD}/lightline/breakingbad.vim ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/breakingbad.vim
 ln -sfn ${PWD}/lualine/breakingbad.lua ~/.vim/plugged/lualine.nvim/lua/lualine/themes/breakingbad.lua
