e0ea8e1a70cbac318ea1575d67292fc12d55f2ed
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2023-10-02 09:00:53 +0200

    undo remap

--- nvim/.config/nvim/lua/kadobot/indent-blankline.lua
+++ nvim/.config/nvim/lua/kadobot/indent-blankline.lua
@@ -1,45 +1,42 @@
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
 require("ibl").setup({
-	buftype_exclude = {
-		"nofile",
-		"terminal",
+	exclude = {
+		filetypes = {
+			"help",
+			"startify",
+			"aerial",
+			"alpha",
+			"dashboard",
+			"packer",
+			"neogitstatus",
+			"NvimTree",
+			"neo-tree",
+			"Trouble",
+		},
 	},
-	filetype_exclude = {
-		"help",
-		"startify",
-		"aerial",
-		"alpha",
-		"dashboard",
-		"packer",
-		"neogitstatus",
-		"NvimTree",
-		"neo-tree",
-		"Trouble",
-	},
-	context_patterns = {
-		"class",
-		"return",
-		"function",
-		"method",
-		"^if",
-		"^while",
-		"jsx_element",
-		"^for",
-		"^object",
-		"^table",
-		"block",
-		"arguments",
-		"if_statement",
-		"else_clause",
-		"jsx_element",
-		"jsx_self_closing_element",
-		"try_statement",
-		"catch_clause",
-		"import_statement",
-		"operation_type",
-	},
-	show_trailing_blankline_indent = false,
-	use_treesitter = true,
-	char = "▏",
-	context_char = "▏",
-	show_current_context = true,
+	indent = { highlight = highlight, char = "▏" },
+	whitespace = { highlight = highlight, remove_blankline_trail = false },
+	scope = { enabled = true },
 })
